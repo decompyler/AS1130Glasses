aff329c8278884cb167e1b2110aeac0c0cd4bbbb
decompyler <randomglitch@gmail.com>
2014-09-08 18:23:21 -0700

    Init

---
+++ messages.h
@@ -0,0 +1,14 @@
+// Scrolling messages
+
+char string0[] PROGMEM = "Josh is a bundle of sticks!";
+char string1[] PROGMEM = "Your message here...  ";
+char string2[] PROGMEM = "Hello World!  ";
+//char string3[] PROGMEM = "  ";
+
+const char * stringArray[] PROGMEM = {
+  string0,
+  string1,
+  string2
+  //string3
+};
+
